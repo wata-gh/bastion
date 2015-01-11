e63a04c2aace20f0dd015668204d0b45db7e9801
wata <wata.gm@gmail.com>
2015-01-11 16:04:05 +0900

    add capistrano local deploy.

--- unicorn.rb
+++ unicorn.rb
@@ -1,11 +1,10 @@
-#@dir = "/home/ec2-user/projects/platform"
 @dir = File.expand_path './', File.dirname(__FILE__)
 
 worker_processes 1
 working_directory @dir
 
 timeout 300
-listen 8010
+listen '/var/socket/bizevo_platform_unicorn.sock', backlog: 1024
 
 pid "#{@dir}/unicorn.pid"
 

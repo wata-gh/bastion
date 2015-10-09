02f1fff16cf5980253206261f91e2066911c6c9d
wata <wata.gm@gmail.com>
2015-10-09 13:42:22 +0900

    fix unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/wata-gh/bastion.git'
 
 set :deploy_to, '/opt/bastion/'
 set :keep_releases, 5
-set :linked_dirs, %w{bin log tmp/pids tmp/cache bundle}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/socket bundle}
 set :unicorn_rack_env, fetch(:stage)
 set :unicorn_config_path, File.join(fetch(:deploy_to), 'current/config/unicorn', "#{fetch(:stage)}.rb")
 

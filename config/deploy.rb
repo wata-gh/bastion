fce1ada7fcddef494fbc4fab7da884e40b2b3ef9
wata <wata.gm@gmail.com>
2015-10-09 13:12:39 +0900

    fix unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:web), in: :sequence, wait: 5 do
       invoke 'unicorn:restart'
     end
   end
